0d0dd9fe112b4e8b49de31902b7112de9e20f230
eurashin <eura.shin@gmail.com>
2019-03-30 21:20:21 -0400

    test code for motor

--- blink_led.py
+++ blink_led.py
@@ -1,22 +1,17 @@
 from GPIOLibrary import GPIOProcessor
 import time
 
+
 GP = GPIOProcessor()
 
 try:
     Pin27 = GP.getPin27()
     Pin27.out()
 
-    Pin29 = GP.getPin29()
-    Pin29.input()	
-
     for i in range(0,20):
-        pinValue = Pin29.getValue();
-
-        if pinValue == 1:
-            Pin27.high()
-        else:
-            Pin27.low()
+        Pin27.high()
+        time.sleep(1)
+        Pin27.low()
         time.sleep(1)
 
 finally:
--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import movement
 import cv2
 
-img = cv2.imread('horse_outline.jpg',0)
+img = cv2.imread('images/uk.png',0)
 movement.image_to_direction(img)
