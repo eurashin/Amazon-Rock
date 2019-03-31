41c998578c92a44a3408d77d2263680408bf343a
eurashin <eura.shin@gmail.com>
2019-03-31 10:22:24 -0400

    please

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import movement
 import cv2
 import pyfirmata_test.py
 
-img = cv2.imread('images/octocat.jpg',0)
+img = cv2.imread('images/square.jpg',0)
 points = movement.image_to_direction(img)
 
 pyfirmata_test.move_rock(points)
